9bf821dbef83f0adbd43ddc9e3177d836a589b10
twice <jbh2032@naver.com>
2022-02-28 01:13:32 +0900

    ㅋㅋㅋ

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,6 +2,7 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("예지야 사랑해");  
+		System.out.println("예지야 사랑해");
+		System.out.println("hello worl!!!");
 	}
 }
