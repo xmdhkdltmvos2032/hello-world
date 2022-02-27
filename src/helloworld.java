548d79fc0dd7e48e65536883333e40b393c1b60a
twice <jbh2032@naver.com>
2022-02-28 01:05:28 +0900

    예지야사랑해

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,6 +2,6 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("hello world!!!!!!!!!!!!");  
+		System.out.println("예지야 사랑해");  
 	}
 }
